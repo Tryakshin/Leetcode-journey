fb0dcb6a0f4d71b8684e71982ebe258e34058914
Curlyxx <120436160+Tryakshin@users.noreply.github.com>
2025-07-09 20:50:04 +1000

    remove spaces in blank line

--- Arrays-Strings/392.py
+++ Arrays-Strings/392.py
@@ -2,7 +2,7 @@ class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
         if len(s) == 0:
             return True
-            
+
         s_index = 0
         for i in t:
             if i == s[s_index]:
